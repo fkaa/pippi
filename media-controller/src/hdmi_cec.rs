da0a3d5766889192f0da97cf49eb7cbcaa38c25f
tmtu <tmtu@tmtu.ee>
2026-02-16 12:18:37 +0100

    use cec-ctl instead

--- media-controller/src/hdmi_cec.rs
+++ media-controller/src/hdmi_cec.rs
@@ -1,15 +1,22 @@
 use cec_rs::{CecConnectionCfgBuilder, CecLogicalAddress, CecDeviceTypeVec, CecDeviceType};
+use std::process::Command;
+use std::io;
+use std::io::Write;
 
 pub fn turn_tv_on() {
-    // cec-client -s -d 1 "as"
-    let conn = CecConnectionCfgBuilder::default()
-        .device_name("test".into())
-        .device_types(CecDeviceTypeVec::new(CecDeviceType::Tv))
-        .build().unwrap().open().unwrap();
-    conn.send_power_on_devices(CecLogicalAddress::Tv).unwrap();
+    // active source
+    send_command(&["-s", "-t0", "--active-source", "phys-addr=2.0.0.0"]);
 }
 pub fn turn_tv_off() {
-    // cec-client -s -d 1 "standby 0"
-    let conn = CecConnectionCfgBuilder::default().build().unwrap().open().unwrap();
-    conn.send_power_on_devices(CecLogicalAddress::Tv).unwrap();
+    // put address 0 in standby
+    send_command(&["-s", "-t0", "--standby"]);
+}
+
+pub fn send_command(args: &[&str]) {
+    let output = Command::new("cec-ctl")
+        .args(args)
+        .output()
+        .expect("failed to start cec-ctl");
+
+    io::stdout().write_all(&output.stdout).unwrap();
 }
